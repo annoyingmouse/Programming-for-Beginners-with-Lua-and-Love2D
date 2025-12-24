43b1c83379e523b2e6afcfbfcdbd50d186000fc6
Dominic Myers <annoyingmouse@gmail.com>
2025-12-24 05:33:59 +0000

    📝 (Letter.lua): Remove unnecessary commented out ASCII art block 📝 (Word.lua): Remove unnecessary commented out ASCII art blocks and clean up code formatting

--- hangman/Letter.lua
+++ hangman/Letter.lua
@@ -1,13 +1,3 @@
---[[
-888              888    888
-888              888    888
-888              888    888
-888      .d88b.  888888 888888 .d88b.  888d888
-888     d8P  Y8b 888    888   d8P  Y8b 888P"
-888     88888888 888    888   88888888 888
-888     Y8b.     Y88b.  Y88b. Y8b.     888
-88888888 "Y8888   "Y888  "Y888 "Y8888  888
-]]
 local Letter = {}
 Letter.__index = Letter
 function Letter:new(char, startX, startY, width)
